1a44ec143a0910cda3119e17b83f2209877f3f20
SIDHARTH SUDHEER <sidharth.sudheer2020a@vitstudent.ac.in>
2022-11-05 11:57:18 +0530

    Add files via upload

---
+++ src/Dashboard/Dealers/Dealers.js
@@ -0,0 +1,68 @@
+import React from 'react'
+import './Dealers.css'
+import img from '../../utils/img.jpg'
+export default function Dealers() {
+  return (
+    <>
+        
+        <div className='Dealers'>
+            <div className="dealer">
+                <img src={img} alt="" />
+                <table className='form'>
+                    <tr><td>Name</td></tr>
+                    <tr><td>Phone no</td></tr>
+                    <tr><td>Category</td></tr>
+                    <tr><td>ID:</td></tr>
+                    <tr><td>Pricing</td></tr>
+                </table>
+                <center><button className='btn'>More</button></center>
+            </div>
+            <div className="dealer">
+                <img src={img} alt="" />
+                <table className='form'>
+                    <tr><td>Name</td></tr>
+                    <tr><td>Phone no</td></tr>
+                    <tr><td>Category</td></tr>
+                    <tr><td>ID:</td></tr>
+                    <tr><td>Pricing</td></tr>
+                </table>
+                <center><button className='btn'>More</button></center>
+            </div>
+            <div className="dealer">
+                <img src={img} alt="" />
+                <table className='form'>
+                    <tr><td>Name</td></tr>
+                    <tr><td>Phone no</td></tr>
+                    <tr><td>Category</td></tr>
+                    <tr><td>ID:</td></tr>
+                    <tr><td>Pricing</td></tr>
+                </table>
+                <center><button className='btn'>More</button></center>
+            </div>
+            <div className="dealer">
+                <img src={img} alt="" />
+                <table className='form'>
+                    <tr><td>Name</td></tr>
+                    <tr><td>Phone no</td></tr>
+                    <tr><td>Category</td></tr>
+                    <tr><td>ID:</td></tr>
+                    <tr><td>Pricing</td></tr>
+                </table>
+                <center><button className='btn'>More</button></center>
+            </div>
+            <div className="dealer">
+                <img src={img} alt="" />
+                <table className='form'>
+                    <tr><td>Name</td></tr>
+                    <tr><td>Phone no</td></tr>
+                    <tr><td>Category</td></tr>
+                    <tr><td>ID:</td></tr>
+                    <tr><td>Pricing</td></tr>
+                </table>
+                <center><button className='btn'>More</button></center>
+            </div>
+        </div>
+    </>
+    
+  )
+}
