153ec11c42e588edb03b2e750a5fcfbfa1cca374
SIDHARTH SUDHEER <sidharth.sudheer2020a@vitstudent.ac.in>
2022-11-12 23:02:51 +0530

    Update CustInfo.js

--- src/Dashboard/CustInfo/CustInfo.js
+++ src/Dashboard/CustInfo/CustInfo.js
@@ -1,38 +1,46 @@
 import React from 'react'
 import {CgProfile} from 'react-icons/cg'
+import Dashboard from '../Dashboard'
 import './CustInfo.css'
 export default function CustInfo() {
   return (
-    <div className='custInfo'>
-        <center><CgProfile size={'30%'}/></center>
-        <center><button className='btn'>Sidharth Sudheer</button></center>
-        <div className='form'>
-          <center>
-            <table>
-            <tr>
-              <td>Phone Number</td>
-              <td><input type="text" /></td>
-            </tr>
-            <tr>
-              <td>Email</td>
-              <td><input type="text" /></td>
-            </tr>
-            <tr>
-              <td>City</td>
-              <td><input type="text" /></td>
-            </tr>
-            <tr>
-              <td>Pincode</td>
-              <td><input type="text" /></td>
-            </tr>
-            <tr>
-              <td colSpan={2}><center><button className='btn'>Confirm and Save</button></center></td>
-            </tr>
-            </table>
-          </center>
-        </div>
-        
-        
-    </div>
+    <>
+      <Dashboard/>
+      <div className="dash_content">
+        <div className='custInfo'>
+                <center><CgProfile size={'30%'}/></center>
+                <center><button className='btn'>Sidharth Sudheer</button></center>
+                <div className='form'>
+                  <center>
+                    <table>
+                    <tr>
+                      <td>Phone Number</td>
+                      <td><input type="text" /></td>
+                    </tr>
+                    <tr>
+                      <td>Email</td>
+                      <td><input type="text" /></td>
+                    </tr>
+                    <tr>
+                      <td>City</td>
+                      <td><input type="text" /></td>
+                    </tr>
+                    <tr>
+                      <td>Pincode</td>
+                      <td><input type="text" /></td>
+                    </tr>
+                    <tr>
+                      <td colSpan={2}><center><button className='btn'>Confirm and Save</button></center></td>
+                    </tr>
+                    </table>
+                  </center>
+                </div>
+                
+                
+            </div>
+      </div>
+      
+    </>
+    
   )
 }
