28dd07a06d02571ed58de5816f7c0133095e2cae
SIDHARTH SUDHEER <sidharth.sudheer2020a@vitstudent.ac.in>
2022-11-12 22:45:11 +0530

    Added Router Branches

--- src/Dashboard/Dealers/Dealers.js
+++ src/Dashboard/Dealers/Dealers.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import './Dealers.css'
 import img from '../../utils/img.jpg'
+import DashNav from '../Dashnav/Dashnav'
+import Dashboard from '../Dashboard'
 export default function Dealers() {
   return (
     <>
-        
+        <Dashboard/>
         <div className='Dealers'>
             <div className="dealer">
-                <img src={img} alt="" />
+                
                 <table className='form'>
                     <tr><td>Name</td></tr>
                     <tr><td>Phone no</td></tr>
@@ -18,7 +20,7 @@ export default function Dealers() {
                 <center><button className='btn'>More</button></center>
             </div>
             <div className="dealer">
-                <img src={img} alt="" />
+                
                 <table className='form'>
                     <tr><td>Name</td></tr>
                     <tr><td>Phone no</td></tr>
